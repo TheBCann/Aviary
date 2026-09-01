a81ce8276acc608b748e70b64a0358ced4909168
TheBCann <161960060+TheBCann@users.noreply.github.com>
2026-08-31 20:08:14 -0400

    Initial Commit

---
+++ Swift-UI-Companion/Swift_UI_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_CompanionApp.swift
+//  Swift-UI-Companion
+//
+//  Created by Brandon Cannizzaro on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
